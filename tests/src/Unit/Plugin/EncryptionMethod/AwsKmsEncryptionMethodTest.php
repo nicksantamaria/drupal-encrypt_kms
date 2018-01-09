756461b610fc7bd59af4c3280ac93cf8a1235a5d
Nick Santamaria <nick.santamaria@previousnext.com.au>
2018-01-09 23:24:33 +1100

    Initial work on unit tests for encrypt/decrypt methods.

--- tests/src/Unit/Plugin/EncryptionMethod/AwsKmsEncryptionMethodTest.php
+++ tests/src/Unit/Plugin/EncryptionMethod/AwsKmsEncryptionMethodTest.php
@@ -4,6 +4,9 @@ namespace Drupal\Tests\encrypt_kms\Unit\Plugin\EncryptionMethod;
 
 use Drupal\Tests\UnitTestCase;
 use Psr\Log\LoggerInterface;
+use Aws\Kms\KmsClient;
+use Aws\Result;
+use Drupal\encrypt_kms\Plugin\EncryptionMethod\AwsKmsEncryptionMethod;
 
 /**
  * Unit tests for AwsKmsEncryptionMethod.
@@ -13,7 +16,12 @@ use Psr\Log\LoggerInterface;
  */
 class AwsKmsEncryptionMethodTest extends UnitTestCase {
 
-  protected $runTestInSeparateProcess = TRUE;
+  /**
+   * The encryption method service.
+   *
+   * @var \Drupal\encrypt\EncryptionMethodInterface|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $encryptionMethod;
 
   /**
    * The logger.
@@ -23,22 +31,72 @@ class AwsKmsEncryptionMethodTest extends UnitTestCase {
   protected $logger;
 
   /**
+   * The KMS Client.
+   *
+   * @var \Aws\Kms\KmsClient|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $kmsClient;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
 
     $this->logger = $this->getMock(LoggerInterface::class);
-
+    $this->$kmsClient = $this->getMock(KmsClient::class)
+      ->disableOriginalConstructor()
+      ->setMethods(['encrypt', 'decrypt'])
+      ->getMock();
+    $this->encryptionMethod = new AwsKmsEncryptionMethod();
+    $this->encryptionMethod->setKmsClient($this->kmsClient);
+    $this->encryptionMethod->setLogger($this->logger);
   }
 
   /**
-   * Tests the test.
+   * Tests encrypt method.
    *
-   * @covers ::foo
+   * @covers ::encrypt
    */
-  public function testFoo() {
-    $this->assert(TRUE);
+  public function testEncrypt() {
+    $result = new Result([
+      'CiphertextBlob' => 'CIPHERTEXT_BLOB',
+    ]);
+
+    $this->kmsClient->expects($this->once())
+      ->method('encrypt')
+      ->withAnyParameters()
+      ->willReturn($result);
+
+    $text = 'the quick brown fox';
+    $key = 'arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab';
+
+    $ciphertext = $this->encryptionMethod->encrypt($text, $key);
+
+    $this->assertEquals('CIPHERTEXT_BLOB', $ciphertext);
+  }
+
+  /**
+   * Tests decrypt method.
+   *
+   * @covers ::decrypt
+   */
+  public function testDecrypt() {
+    $result = new Result([
+      'Plaintext' => 'the quick brown fox',
+    ]);
+
+    $this->kmsClient->expects($this->once())
+      ->method('decrypt')
+      ->withAnyParameters()
+      ->willReturn($result);
+
+    $ciphertext = 'CIPHERTEXT_BLOB';
+    $key = 'arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab';
+
+    $text = $this->encryptionMethod->decrypt($ciphertext, $key);
+
+    $this->assertEquals($text, $ciphertext);
   }
 
 }
